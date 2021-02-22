5bf63d54d372114bf3b68165aa2201dcda722838
n3utrinoCH <zirn.pascal@gmail.com>
2021-02-22 22:07:06 +0100

    initial

---
+++ gameBubbles/bubblePop.go
@@ -0,0 +1,15 @@
+components {
+  id: "bubblePop"
+  component: "/particles/bubblePop.particlefx"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
