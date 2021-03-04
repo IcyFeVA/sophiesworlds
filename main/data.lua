7678fa3c931e08929a4f4166d298becf6eaaff10
n3utrinoCH <zirn.pascal@gmail.com>
2021-03-04 12:01:19 +0100

    xylo sounds refactoring moved func tablelength to data.lua

--- main/data.lua
+++ main/data.lua
@@ -3,6 +3,14 @@ local M = {}
 M.SW = 960
 M.SH = 540
 
-
+M.tablelength = function (T)
+	local count = 0
+	for k,v in pairs(T) do
+		if v then
+			count = count + 1 
+		end 
+	end
+	return count
+end
 
 return M
